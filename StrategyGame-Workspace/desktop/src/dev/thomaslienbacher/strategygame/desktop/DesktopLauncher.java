31fe13e5bcd62ab48d1361a82ab02ce4d547b850
thomaslienbacher <lienbacher.tom@gmail.com>
2018-08-03 13:13:48 +0200

    Initial commit

---
+++ StrategyGame-Workspace/desktop/src/dev/thomaslienbacher/strategygame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.thomaslienbacher.strategygame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.thomaslienbacher.strategygame.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
